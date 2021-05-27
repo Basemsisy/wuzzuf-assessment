8afc31be17957d9d6e9716e34e190a8ea59dae57
basem <basemsisy@gmail.com>
2021-05-27 17:05:45 +0200

    refactor[Job]: use custom hook

--- src/app/pages/Job/Job.tsx
+++ src/app/pages/Job/Job.tsx
@@ -1,9 +1,9 @@
 import React, { FunctionComponent } from "react";
-import RelatedCard from "app/components/RelatedCard";
 import { getJobRelatedSkills } from "app/store/main/actions";
 import styles from "./Job.module.scss";
 import Sppinner from "app/components/Sppinner";
 import useAction from "app/hooks/useAction";
+import RelatedWrapper from "app/components/RelatedWrapper";
 
 interface Props {
   match: { params: { id: string } };
@@ -14,18 +14,13 @@ const Job: FunctionComponent<Props> = ({
     params: { id },
   },
 }) => {
-  const { isLoading, data: jobDetails } = useAction(getJobRelatedSkills, id)
+  const { isLoading, data: jobDetails } = useAction(getJobRelatedSkills, id);
 
-  if (isLoading) return <Sppinner />
+  if (isLoading) return <Sppinner />;
   return (
     <div className={styles.Job}>
       <h4>{jobDetails?.job_title}</h4>
-      <div className={styles.RelatedWrapper}>
-        <h5>related skills:</h5>
-        {jobDetails?.skills?.map((skill: any) => (
-          <RelatedCard key={skill.skill_uuid} type="skill" data={skill} />
-        ))}
-      </div>
+      <RelatedWrapper type="skill" data={jobDetails?.skills} />
     </div>
   );
 };
