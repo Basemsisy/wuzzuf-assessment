53961d65287f33c30b2384c6801a615a8be83918
basem <basemsisy@gmail.com>
2021-05-27 16:35:57 +0200

    refactor[RelatedCard]: make it more reusable

--- src/app/components/RelatedCard/RelatedCard.tsx
+++ src/app/components/RelatedCard/RelatedCard.tsx
@@ -3,34 +3,41 @@ import { Link } from "react-router-dom";
 import styles from "./RelatedCard.module.scss";
 
 interface Props {
-  data: {
-    title: string;
-    description: string
-  }
+  type: "job" | "skill";
+  data: any;
 }
 
-const RelatedCard: FunctionComponent<Props> = ({ data }) => {
+const RelatedCard: FunctionComponent<Props> = ({ data, type }) => {
+  const isSkillCard = type === "skill";
   return (
     <div className={styles.RelatedCard}>
       <h5>
-        <Link to="/skill">{data.title}</Link>
+        <Link
+          to={
+            isSkillCard ? `/skill/${data.skill_uuid}` : `/job/${data.job_uuid}`
+          }
+        >
+          {isSkillCard ? data.skill_name : data.job_title}
+        </Link>
       </h5>
 
-      {data.description && <p>
-        {data.description}
-      </p>}
+      {isSkillCard && <p>{data.description}</p>}
       <ul className={styles.Properties}>
+        {isSkillCard && (
+          <li className={styles.Properties__Item}>
+            <b>type: </b>
+            <span>{data.skill_type}</span>
+          </li>
+        )}
+
         <li className={styles.Properties__Item}>
-          <b>type: </b>
-          <span>test</span>
+          <b>importance: </b>
+          <span>{data.importance}</span>
         </li>
+
         <li className={styles.Properties__Item}>
-          <b>type: </b>
-          <span>test</span>
-        </li>
-        <li className={styles.Properties__Item}>
-          <b>type: </b>
-          <span>test</span>
+          <b>level: </b>
+          <span>{data.level}</span>
         </li>
       </ul>
     </div>
