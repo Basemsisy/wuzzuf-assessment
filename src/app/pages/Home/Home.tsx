b2cea1ca241d8cfeb670eee134ea5a2a29003f5f
basem <basemsisy@gmail.com>
2021-05-27 04:08:32 +0200

    feat[/] global

--- src/app/pages/Home/Home.tsx
+++ src/app/pages/Home/Home.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import styles from "./Home.module.scss";
+import JobCard from 'app/components/JobCard'
 
 const Home = ({ ...props }) => {
   return (
     <div className={styles.Home}>
-      <p>home page</p>
+      <div className="container">
+        <h4>all jobs(220)</h4>
+        <div className="grid">
+          <JobCard />
+          <JobCard />
+          <JobCard />
+          <JobCard />
+        </div>
+      </div>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
+import "./index.scss";
 import App from "./app/App";
 import { Provider } from "react-redux";
 import { configureStore } from "./app/store/config";
