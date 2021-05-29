17469127c4cd01645e081403386762b9e2203eb3
basem <basemsisy@gmail.com>
2021-05-29 09:52:38 +0200

    fix[useAction]: add canUpdate option

--- src/app/hooks/useAction.tsx
+++ src/app/hooks/useAction.tsx
@@ -1,26 +1,33 @@
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 
-const useAction = (action: any, params?: any) => {
+const useAction = (action: any, params?: any, canUpdate: boolean = false) => {
   const dispatch = useDispatch();
-  const [state, setState] = useState<{ isLoading: boolean, data: any, error: any }>({ data: null, isLoading: false, error: null });
+  const [state, setState] = useState<{
+    isLoading: boolean;
+    data: any;
+    error: any;
+  }>({ data: null, isLoading: true, error: null });
 
   const fetchData = async () => {
-    setState({ ...state, isLoading: true })
+    setState({ ...state, isLoading: true });
     try {
       const result = await dispatch(action(params));
-      setState({ ...state, isLoading: false, data: result.data })
-
+      setState({ ...state, isLoading: false, data: result.data });
     } catch (error) {
-      setState({ ...state, error })
+      setState({ ...state, error });
     }
   };
-  useEffect(() => {
-    fetchData();
-  }, []);
+
+  useEffect(
+    () => {
+      fetchData();
+    },
+    canUpdate ? [params] : []
+  );
 
   return {
-    ...state
+    ...state,
   };
 };
 
